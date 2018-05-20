33de94cee067bd2436aee6041a2d4840840b180f
mtbui-johnny <bmtoan@gmail.com>
2018-05-20 17:11:02 +0800

    1

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    AUTH0_DOMAIN: 'bmtoan.auth0.com', // e.g., kmaida.auth0.com
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://bmtoan1989:1989toan@ds147920.mlab.com:47920/bmtoan',
+    NAMESPACE: 'http://myapp.com/roles'
+  };
